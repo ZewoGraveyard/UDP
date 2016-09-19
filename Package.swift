a63436d1774c95df565f7e343c62f23390e369c1
Romain Bossart <bosr@users.noreply.github.com>
2016-09-19 22:21:22 +0200

    update to 3.0 (#10)
    * update to 09-11a
    
    * socket.receive(_:timingOut) -> socket.receive(upTo:timingOut)
    
    for consistency with TCP
    
    * update to 3.0
    
    * style & interface (cf review in ZewoGraveyard/Flux)
    
    * update .travis.yml (use recent Scripts/Travis/install.sh)
    
    * cosmetic changes for packaging
    
    * remove swift suffix on .swift-version
    
    * minor style nit
    
    * fix typo

--- Package.swift
+++ Package.swift
@@ -3,6 +3,11 @@ import PackageDescription
 let package = Package(
     name: "UDP",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/IP.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/Zewo/IP.git", majorVersion: 0, minor: 13),
+
+
+        // test-only dependencies (not yet available in swiftpm: SE-0019)
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 13),
     ]
 )
